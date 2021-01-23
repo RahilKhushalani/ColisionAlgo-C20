3f2ce5fe0abe4c633755d5e35b9147eed1018713
RahilKhushalani <76523437+RahilKhushalani@users.noreply.github.com>
2021-01-23 18:50:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var movingRect, fixedRect
+
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor = "blue";
+  fixedRect.debug = true;
+
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "blue";
+  movingRect.debug = true;
+
+
+
+}
+
+function draw() {
+  background(0); 
+
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  //console.log(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2)
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    fixedRect.shapeColor = "yellow";
+    movingRect.shapeColor = "yellow";
+  }
+  else{
+    fixedRect.shapeColor = "blue";
+    movingRect.shapeColor = "blue";
+
+  }
+  
+  
+
+  drawSprites();
+}
